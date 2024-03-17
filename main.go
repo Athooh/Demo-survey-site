9dc6ccfd63a44937f4e8e9da2be6a2a315d32184
odhiambo_athooh <s.athooh@gmail.com>
2024-03-18 01:00:18 +0300

    mysql db added

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"text/template"
+
+	_ "github.com/go-sql-driver/mysql" // mysql
 )
 
 var tmpl *template.Template
+
+var db *sql.DB
+
+func getMySQLDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/demosurveysitedb?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
 
 func init() {
 	tmpl = template.Must(template.ParseFiles("./template/form.html"))
@@ -24,6 +39,7 @@ type studentSurvey struct {
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
+	db = getMySQLDB()
 	if r.Method != http.MethodPost {
 		tmpl.Execute(w, nil)
 		return
@@ -33,16 +49,31 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		Name:            r.FormValue("name"),
 		Email:           r.FormValue("email"),
 		Age:             r.FormValue("age"),
-		Role:            r.FormValue("current-role"),
-		FavoriteFeature: r.FormValue("radio-buttons"),
-		Recommendation:  []string{strings.Join(r.Form["improvements"], ", ")},
+		Role:            r.FormValue("current_role"),
+		FavoriteFeature: r.FormValue("favorite_features"),
+		Recommendation:  r.Form["improvements"],
 		Comments:        r.FormValue("comment"),
 	}
+
+	recommendations := ""
+	if len(student.Recommendation) > 0 {
+		recommendations = strings.Join(student.Recommendation, ", ")
+	}
+
 	if r.FormValue("submit") == "Submit" {
-		tmpl.Execute(w, struct {
-			Success bool
-			Message string
-		}{Success: true, Message: "Congratulations!!! Survey Complete"})
+		age, _ := strconv.Atoi(student.Age)
+		_, err := db.Exec("insert into student (name, email, age, current_role, favorite_features, improvements, comment) values(?, ?, ?, ?, ?, ?, ?)", student.Name, student.Email, age, student.Role, student.FavoriteFeature, recommendations, student.Comments)
+		if err != nil {
+			tmpl.Execute(w, struct {
+				Success bool
+				Message string
+			}{Success: true, Message: err.Error()})
+		} else {
+			tmpl.Execute(w, struct {
+				Success bool
+				Message string
+			}{Success: true, Message: "Congratulations!!! Survey Complete"})
+		}
 	}
 	fmt.Println(student)
 }
